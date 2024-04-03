61556538eb86c0cfe59a96c9058c250714cceb21
B1NT0N <kleiton1002andrade@gmail.com>
2024-04-03 17:32:10 -0100

    Implemeted attendee repository

---
+++ src/models/repository/attendees_repository_test.py
@@ -0,0 +1,26 @@
+import pytest
+from .attendees_repository import AttendeesRepository
+from src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="New registry")
+def test_insert_attendee():
+    event_id = "UUID"
+    attendee_info = {
+        "uuid": "UUID ATT",
+        "name": "NAME",
+        "email": "EMAIL@EMAIL.COM",
+        "event_id": event_id
+    }
+
+    attendees_repository = AttendeesRepository()
+    response = attendees_repository.insert_attendee(attendee_info)
+    print(response)
+    
+@pytest.mark.skip(reason="...")    
+def test_get_attendee_badge_by_id():
+    attendee_id = "UUID ATT"
+    attendees_repository = AttendeesRepository()
+    attendee = attendees_repository.get_attendee_badge_by_id(attendee_id)
+    print(attendee)
--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -5,20 +5,20 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
 class EventsRepository:
-    def insert_event(self, eventsInfo: Dict) -> Dict:
+    def insert_event(self, events_info: Dict) -> Dict:
         try:
             with db_connection_handler as database:
                 event = Events(
-                    id=eventsInfo.get("uuid"),
-                    title=eventsInfo.get("title"),
-                    details=eventsInfo.get("details"),
-                    slug=eventsInfo.get("slug"),
-                    maximum_attendees=eventsInfo.get("maximum_attendees"),
+                    id=events_info.get("uuid"),
+                    title=events_info.get("title"),
+                    details=events_info.get("details"),
+                    slug=events_info.get("slug"),
+                    maximum_attendees=events_info.get("maximum_attendees"),
                 )
                 database.session.add(event)
                 database.session.commit()
 
-                return eventsInfo
+                return events_info
         except IntegrityError:
             raise Exception("Event already registred")
         
